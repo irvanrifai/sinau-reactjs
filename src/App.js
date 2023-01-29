1d494ea08d38e3a6857ab70bb5c29c34be17d008
Irvan Rifaí <ivan.rivai6921@gmail.com>
2023-01-29 12:21:00 +0700

    init sinau

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>Irvan Rifa'i</h1>
+        {/* <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div> */}
+        <button type="button" class="btn btn-warning">Warning</button>
+        <h2>Test</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
